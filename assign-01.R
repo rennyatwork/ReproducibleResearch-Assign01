27f75e478bc2bf314337f82db373def7a985e5cb
rennyatwork <rennyatwork@gmail.com>
2014-10-29 20:10:59 -0400

    adding new files

---
+++ assign-01.R
@@ -0,0 +1,98 @@
+##initialize to use library sqldf
+initialize <-function ()
+{
+  library(sqldf)
+  library(ggplot2)
+}
+
+##This function return the full path+ file name to be read
+getFullPathFileName <-function(pFileName)
+{
+  return (paste(paste(getwd(),paste("repdata_data_activity", pFileName, sep="/"), sep="/"), ".csv" , sep=""))
+}
+
+## Gets the Steps Dt
+#This function reads the csv file into a datatable
+getDtSteps <- function()
+{ 
+  if(!exists("dtSteps"))
+  {
+    dtSteps <<- read.csv(getFullPathFileName('activity'), header=T, sep=",")
+  }
+  dtSteps$steps <- as.numeric (dtSteps$steps)
+  dtSteps$date <- as.Date(dtSteps$date, "%Y-%m-%d")    
+  return (dtSteps);  
+}
+
+
+
+##Gets a datatable without NA values
+getDtStepsWithoutNa <- function()
+{
+  dtSteps <- getDtSteps()  
+  return (
+sqldf("SELECT 
+        *
+        FROM dtSteps 
+        WHERE steps <>'NA'
+        ")
+)
+}
+
+## Gets the plotting dt
+# groupb by day
+getPlotDtStepsWithoutNa <-function()
+{
+  dtSteps<- getDtStepsWithoutNa()
+  return( sqldf("SELECT 
+        date
+        ,sum(steps) as totalSteps
+        ,avg(steps) as avgSteps
+        ,median(steps) as medianSteps
+         
+        FROM
+        dtSteps
+        GROUP BY date
+        ")
+  )
+}
+
+plotHistogramWithoutNa <-function()
+{
+  dtPlot <- getPlotDtStepsWithoutNa()
+  #dtPlot <- sqldf("select date, totalSteps from dtPlot")
+  #qplot(dtPlot$date, dtPlot$sumSteps) +geom_histogram()
+  ggplot(data=dtPlot, aes(x=date, y=totalSteps)) + geom_bar(stat="identity", colour="black")
+  #_ggplot(data=dtPlot, aes(date)) + geom_bar(aes(y=totalSteps), stat="identity") + geom_line(aes(y=avgSteps, color="blue"))
+  
+  ##avg steps:
+  #ggplot(data=dtPlot, aes(x=date, y=avgSteps)) + geom_bar(stat="identity", colour="blue")
+}
+
+#average steps without NA
+getAverageStepsPerDay <-function()
+{
+  return(mean(getPlotDtStepsWithoutNa()$totalSteps))
+}
+
+
+#median steps without NA
+getMedianStepsPerDay <-function()
+{
+  return (median(getPlotDtStepsWithoutNa()$totalSteps)  )
+}
+
+
+# x <- as.data.frame(c(1,2,3,4,5))
+# colnames(x) <- c("x")
+# sqldf("select median(x) from x")
+# lapply(x, class)
+
+##how many unique dates not NA?
+#length (unique(subset(dtSteps, !is.na(steps))$date ))
+
+## how many unique dates NA?
+#length (unique(subset(dtSteps, is.na(steps))$date ))
+
+##total number of missing values
+# nrow (subset(dtSteps, is.na(steps) ))
